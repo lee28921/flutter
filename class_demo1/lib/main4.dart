7dd23345e129e8d14b0e7c154bbb10f132caf886
dlwlals6790 <dlwlals6790@gmail.com>
2024-01-30 17:10:42 +0900

    변수와 연산자 등등

---
+++ class_demo1/lib/main4.dart
@@ -0,0 +1,23 @@
+
+// 코드의 시작점
+void main() {
+
+  int point = 90;
+  
+  if(point >= 90){
+    print('A학점');
+  } else if(point >= 80) {
+    print('b학점');
+  } else if(point >= 70) {
+    print('C학점');
+  } else {
+    print('F학점');
+  }
+
+  print("------------");
+  // 삼항 연산자
+  // 조건식 ? 결과1 : 결과2
+  print(point >= 60 ? "합격" : "불합격");
+
+}
+
