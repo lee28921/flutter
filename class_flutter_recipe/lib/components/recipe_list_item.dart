db3c98b98fea899add34dbe35ea4d9613507755a
dlwlals6790 <dlwlals6790@gmail.com>
2024-02-14 20:43:22 +0900

    body 일부 구현

--- class_flutter_recipe/lib/components/recipe_list_item.dart
+++ class_flutter_recipe/lib/components/recipe_list_item.dart
@@ -8,8 +8,28 @@ class RecipeListItem extends StatelessWidget {
   const RecipeListItem(
       {required this.imageName, required this.title, super.key});
 
+  // build 함수가 호출 되면 화면에 그림을 그려준다
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Image.asset(
+            "assets/images/$imageName.jpeg",
+            fit: BoxFit.cover,
+          ),
+          SizedBox(height: 10),
+          Text(
+            title,
+            style: TextStyle(fontSize: 20),
+          ),
+          Text("Have you ever made your own $title? "
+              "Once you've tried a homemade $title, "
+              "you'll never go back.")
+        ],
+      ),
+    );
   }
 }
--- class_flutter_recipe/lib/components/recipe_menu.dart
+++ class_flutter_recipe/lib/components/recipe_menu.dart
@@ -3,8 +3,47 @@ import 'package:flutter/material.dart';
 class RecipeMenu extends StatelessWidget {
   const RecipeMenu({super.key});
 
+  // build 화면에다가 그림을 그려주는 함수
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.only(top: 30),
+      child: Row(
+        children: [
+          _buildMenuItem(Icons.food_bank_outlined, "ALL"),
+          const SizedBox(width: 25),
+          _buildMenuItem(Icons.coffee, "Coffee"),
+          const SizedBox(width: 25),
+          _buildMenuItem(Icons.fastfood_outlined, "Burger"),
+          const SizedBox(width: 25),
+          _buildMenuItem(Icons.local_pizza, "Pizza"),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildMenuItem(IconData food, String text) {
+    return Container(
+        width: 60,
+        height: 80,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(30),
+          border: Border.all(color: Colors.black12),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              food,
+              color: Colors.redAccent,
+              size: 30,
+            ),
+            SizedBox(height: 5),
+            Text(
+              text,
+              style: TextStyle(color: Colors.black87),
+            )
+          ],
+        ));
   }
 }
